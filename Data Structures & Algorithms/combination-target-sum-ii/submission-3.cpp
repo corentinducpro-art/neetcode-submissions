5d81d5ed9483f147ad1208c5bd8b6a0d3ac3db38
corentinducpro-art <corentinduc.pro@gmail.com>
2026-06-25 15:22:50 +0200

    Add: combination-target-sum-ii - submission-3

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-3.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+  std::vector<int> bag;
+  std::vector<std::vector<int>> solution;
+  std::sort(candidates.begin(), candidates.end());
+  recursiveTree(0, target, 0, candidates, solution, bag);
+
+  return solution;
+    }
+private:
+  std::set<std::vector<int>> combinationAlreadyUse;
+    void recursiveTree(size_t depth, int target, int currentTarget,
+                             std::vector<int> &candidates,
+                             std::vector<std::vector<int>> &solution,
+                             std::vector<int> &bag) {
+
+
+
+
+  if (currentTarget == target) {
+    solution.push_back(bag);
+    return;
+  }
+
+  if (depth == candidates.size() || currentTarget > target)
+    return;
+
+  // Take it
+  bag.push_back(candidates[depth]);
+  recursiveTree(depth + 1, target, currentTarget + candidates[depth],
+                candidates, solution, bag);
+  bag.pop_back();
+
+  // Don't take it
+  while (depth < (candidates.size() - 1) &&
+         candidates[depth] == candidates[depth + 1])
+    depth++;
+  recursiveTree(depth + 1, target, currentTarget, candidates, solution, bag);
+
+  return;
+}
+};
